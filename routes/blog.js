1ecf380e32ec7fd8dccd6aa3ef47d5dc385aaa04
Jonathan Cain <jcainuk@gmail.com>
2022-07-14 16:27:56 +0100

    begin setting up routes in blog.js and practised a redirect up routes

---
+++ routes/blog.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.redirect('/posts');
+});
+
+router.get('/posts', (req, res) => {
+  res.render('posts-list');
+});
+
+module.exports = router;
