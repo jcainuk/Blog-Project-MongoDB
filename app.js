d9c820317565a9332e3093947e6291158512f2b3
Jonathan Cain <jcainuk@gmail.com>
2022-07-14 16:30:44 +0100

    add blogRoutes Middleware to app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = require('express');
 const path = require('path');
 
 // Require routes here
+const blogRoutes = require('./routes/blog');
 
 // Initiate app
 const app = express()
@@ -12,6 +13,7 @@ app.request('view engine', 'ejs');
 app.request('views', path.join(__dirname, 'views'));
 
 // Middleware app.use()
+app.use(blogRoutes);
 
 // Listen
 app.listen(3000);
