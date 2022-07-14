fbd74f5f6c3d4e1024aee2823df9881ce4897840
Jonathan Cain <jcainuk@gmail.com>
2022-07-14 15:43:24 +0100

    begin express set up

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
+// Initial set- up
 const app = require('express');
 const path = require('path');
+
+// Require routes here
+
+// Initiate app
+const app = express()
+
+// Activate EJS View Engines
+app.request('view engine', 'ejs');
+app.request('views', path.join(__dirname, 'views'));
+
+// Middleware app.use()
+
+// Listen
+app.listen(3000);
