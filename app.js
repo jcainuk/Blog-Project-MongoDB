ce64be8a2aaa02595e5702482282cef9b9e0aab9
Jonathan Cain <jcainuk@gmail.com>
2022-07-14 16:33:57 +0100

    fix a type importing express variable. Changed method to app.set() for EJS view engines as that wasn't working either!

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 // Initial set- up
-const app = require('express');
+const express = require('express');
 const path = require('path');
 
 // Require routes here
 const blogRoutes = require('./routes/blog');
 
 // Initiate app
-const app = express()
+const app = express();
 
 // Activate EJS View Engines
-app.request('view engine', 'ejs');
-app.request('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 // Middleware app.use()
 app.use(blogRoutes);
