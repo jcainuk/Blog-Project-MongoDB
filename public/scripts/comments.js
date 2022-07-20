5e596c3b29c520bedb097db3c26439ea3b2d7cb2
Jonathan Cain <jcainuk@gmail.com>
2022-07-20 15:31:32 +0100

    refactor fetchCommentsForPost so that response data is saved in a variable and then parsed as json

--- public/scripts/comments.js
+++ public/scripts/comments.js
@@ -2,7 +2,10 @@ const loadCommentsBtnElement = document.getElementById('load-comments-btn');
 
 const fetchCommentsForPost = async () => {
   const postId = loadCommentsBtnElement.dataset.postid;
-  await fetch(`/posts/${postId}/comments`);
+
+  const response = await fetch(`/posts/${postId}/comments`);
+
+  const data = await response.json();
 };
 
 loadCommentsBtnElement.addEventListener('click', fetchCommentsForPost);
